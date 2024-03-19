35a4eabe30d597695f0e68f27ff85924e4732009
Gaudencio Jurado <gjurado@virtualidevs.com>
2024-03-19 16:30:52 -0600

    Stop tracking ignored files

--- src/routes/commentRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getComments } from '../controllers/commentsController';
-import { verifyToken } from '../middleware/verifyToken';
-
-const router = Router();
-
-router.get('/:videoId', verifyToken, getComments);
-
-export default router;
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import config from './config';
-import app from './app';
-
-const PORT = config.port;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
